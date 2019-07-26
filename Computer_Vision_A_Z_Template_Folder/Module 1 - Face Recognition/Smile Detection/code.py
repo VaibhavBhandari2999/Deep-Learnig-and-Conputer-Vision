16f013859f55069dfbcf56538b0750d7d87515b4
Vaibhav <vbhandari2999@gmail.com>
2019-07-26 13:12:19 +0530

    HomeWork Full

--- Computer_Vision_A_Z_Template_Folder/Module 1 - Face Recognition/Smile Detection/code.py
+++ Computer_Vision_A_Z_Template_Folder/Module 1 - Face Recognition/Smile Detection/code.py
@@ -1,4 +1,4 @@
-# Homework Solution
+#Similar to face and eyes detection, except we include another cascade for smile and search for smiles in the same way we did for eyes(inside the face rectangular)
 
 # Importing the libraries
 import cv2
@@ -18,7 +18,8 @@ def detect(gray, frame):
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)                  #If we take the same arguements as face and eye, we get many wrongly detected smiles in the rectangle. So instead we change the third arguement(minimum number of neighbouring zones which are accepted, as more zones need ot be accepted for an accurate smile to be detecetd.)
+                                                                                    #Also the second arguement(the factor by which the image is reduced or filters are accepted) is increased to 1.7
         for (sx, sy, sw, sh) in smiles:
             cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
     return frame
